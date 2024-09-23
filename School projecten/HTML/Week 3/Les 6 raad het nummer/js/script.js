75f30c17c5908f4a7f9e85e1267e6f6353a51f28
Ebe133 <6050520@mborijnland.nl>
2024-09-23 14:29:01 +0200

    Add files via upload

---
+++ School projecten/HTML/Week 3/Les 6 raad het nummer/js/script.js
@@ -0,0 +1,10 @@
+
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+function guess() {
+   
+   const input =document.getElementById("input")
+}
+
+let userGuess = parseInt(input.value);
+
